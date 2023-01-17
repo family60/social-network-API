eda26f500b61ca4c9abbedbbf4ddb5adbf9d2ff7
Kush Patel <kushrpatel06@yahoo.com>
2023-01-17 13:58:38 -0500

    Started work on Models for MongoDB Created User.js, Reaction.js, Thought.js, and index.js files Finished User model

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+
+//Schema to create User model
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, "Must be a valid email address!"]
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: "Thought"
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }]
+},
+{toJSON: {
+    virtuals: true
+},
+id: false}
+);
+
+//virtual to get a count of friends that the user has
+userSchema.virtual("friendCount").get(function(){
+    return this.friends.length;
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
